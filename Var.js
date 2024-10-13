e9a34b8a907d76940276f113b9535f79f4af5469
Mohammed SHAMKHI <theshamkhi1@gmail.com>
2024-10-14 00:31:02 +0100

    Var

---
+++ Var.js
@@ -0,0 +1,60 @@
+let prenom = "Mohammed";
+console.log(prenom);
+
+///////////////////////////
+let a = 11;
+let b = 7;
+console.log(a+b);
+console.log(a-b);
+console.log(a*b);
+console.log(a/b);
+
+///////////////////////////
+let nom = "CHAMKHI";
+let nomComplet = nom + " " + prenom;
+console.log(nomComplet);
+
+///////////////////////////
+let age = 25;
+let majeur = age >= 18;
+console.log(majeur);
+
+///////////////////////////
+let x = 11;
+let y = 7;
+console.log("x =", x, ", y =", y);
+
+// Échange des valeurs
+x = x + y;
+y = x - y;
+x = x - y;
+console.log("x =", x, ", y =", y);
+
+///////////////////////////
+let rayon = 3;
+const π = 3.14;
+console.log(2 * π * rayon);
+
+///////////////////////////
+let celsius = 22;
+let F = celsius * 9/5 + 32;
+console.log(F);
+
+//////////////////////////
+let anneeNaissance = 1999;
+let anneeActuelle = 2024;
+let Age = anneeActuelle - anneeNaissance;
+console.log(Age);
+
+/////////////////////////
+let longeur = 3;
+let largeur = 4;
+let P = 2 * (longeur + largeur);
+console.log(P);
+
+/////////////////////////
+let note1 = 11;
+let note2 = 7;
+let note3 = 3;
+let M = (note1 + note2 + note3)/3;
+console.log(M);
